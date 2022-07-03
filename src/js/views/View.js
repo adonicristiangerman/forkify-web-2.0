3b7b4c9eef3b3020b8c93e922ae40d6f5567d43b
Adoni Cristian <adoni.cristiang@gmail.com>
2022-07-03 17:13:29 -0400

    added babel

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,14 @@ import icons from '../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   *Render the recived object to the DOM
+   * @param {Object | Object[]} data The data to be render (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render is false
+   * @this {Object} View instance
+   * @author Adoni Cristian
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
